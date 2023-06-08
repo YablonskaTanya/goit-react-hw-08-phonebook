278626f1be0fd966600524cbb7ffe4dadda8acc3
Татьяна Яблонская <t.n.yablonskaya@gmail.com>
2023-06-08 23:43:50 +0300

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,7 +2,7 @@ import AuthNav from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import UserMenu from 'components/UserMenu/UserMenu';
 import { useAuth } from 'hooks';
-import css from './AppBar.mpdule.css';
+// import css from './AppBar.mpdule.css';
 
 export default function AppBar() {
   const { isLoggedIn } = useAuth();
