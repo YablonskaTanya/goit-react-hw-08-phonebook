31e018b51a5849ae8bb34cff8c163f6c93801713
Татьяна Яблонская <t.n.yablonskaya@gmail.com>
2023-06-09 09:41:40 +0300

    add fix

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -19,21 +19,22 @@ export default function Contacts() {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'start',
-        // marginLeft: 'auto',
-        // marginRight: 'auto',
-        // width: '500px',
-        // marginTop: '50px',
-        // backgroundColor: '#b4d6f5',
-        // borderRadius: '8px',
-        // padding: '40px 20px',
-        // outline: 'auto #291578',
-        // outlineOffset: '-10px',
       }}
     >
       <ContactForm />
       <Filter />
       <title>Contacts</title>
-      {isLoading && !error && <b>Loading...</b>}
+      {isLoading && !error && (
+        <b
+          style={{
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginTop: '10px',
+          }}
+        >
+          Loading...
+        </b>
+      )}
       <ContactList />
     </div>
   );
--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,74 +1,6 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-// console.log(axios.defaults.baseURL);
-
-// const setAuthHeader = token => {
-//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
-
-// const clearAuthHeader = () => {
-//   axios.defaults.headers.common.Authorization = '';
-// };
-
-// export const register = createAsyncThunk(
-//   '/users/signup',
-//   async (credentials, thunkAPI) => {
-//     try {
-//       const res = await axios.post('/users/signup', credentials);
-//       setAuthHeader(res.data.token);
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const logIn = createAsyncThunk(
-//   'auth/login',
-//   async (credentials, thunkAPI) => {
-//     try {
-//       const res = await axios.post('/users/login', credentials);
-//       setAuthHeader(res.data.token);
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
-//   try {
-//     await axios.post('/users/logout');
-
-//     clearAuthHeader();
-//   } catch (error) {
-//     return thunkAPI.rejectWithValue(error.message);
-//   }
-// });
-
-// export const refreshUser = createAsyncThunk(
-//   'auth/refresh',
-//   async (_, thunkAPI) => {
-//     const state = thunkAPI.getState();
-//     const persistedToken = state.auth.token;
-
-//     if (persistedToken === null) {
-//       return thunkAPI.rejectWithValue('Unable to fetch user');
-//     }
-
-//     try {
-//       setAuthHeader(persistedToken);
-//       const res = await axios.get('/users/me');
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import Notiflix from 'notiflix';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
@@ -89,7 +21,7 @@ export const register = createAsyncThunk(
       setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
-      console.log(error);
+      Notiflix.Notify.warning('Something went wrong. Please, try again .');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -104,7 +36,7 @@ export const logIn = createAsyncThunk(
       setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
-      console.log(error);
+      Notiflix.Notify.warning('Something went wrong. Please, try again .');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
